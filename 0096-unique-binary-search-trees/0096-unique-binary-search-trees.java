c250471b91f2427ee460e25e6ff1855f4805a736
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-02-28 12:38:23 +0530

    Time: 0 ms (100%), Space: 42.4 MB (12.82%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numTrees(int n) {
+        int [] dp = new int [n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+    }
+
+    public int solve(int n , int [] dp){
+        if(n == 0 || n == 1 ){
+            return 1;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int ans = 0;
+        for(int i = 0; i < n ; i++){
+            int lefttree = solve(i,dp);
+            int righttree = solve(n-i-1,dp);
+            ans += lefttree * righttree;
+        }
+
+        return dp[n] = ans;
+    }
+}
