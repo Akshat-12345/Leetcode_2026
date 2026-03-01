0223a5d8bc8680e61b6492821ff1bf936187f4be
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-03-01 21:39:39 +0530

    Time: 2 ms (12.26%), Space: 46.3 MB (53.35%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -7,7 +7,7 @@ class Solution {
 
         for(int i = 1 ; i <= n ; i++){
            int buyPrice = prices[i-1];
-           dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] - buyPrice);
+           dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] - buyPrice); // tum kitna khareed rhe ho , hold,nothold - price
            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0] + buyPrice);
         }
 
