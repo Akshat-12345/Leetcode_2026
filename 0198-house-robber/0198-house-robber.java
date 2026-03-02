32552dad366e2605ee3b7c4913a461b8d4577bc1
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-03-02 16:31:47 +0530

    Time: 0 ms (100%), Space: 42.7 MB (75.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,27 +1,37 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int [] dp = new int [n];
-        Arrays.fill(dp,-1);
+        int [] dp = new int [n+1];
+        // Arrays.fill(dp,-1);
 
-        return solve(0,nums,dp);
-    }
-
-    public int solve(int i , int [] nums , int [] dp){
-        int n = nums.length;
-        if(i >= n){
-            return 0;
+        // return solve(0,nums,dp);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 2 ; i <= n ; i++){
+            int val = nums[i-1];
+            dp[i] = Math.max(dp[i-1],dp[i-2] + val);
         }
 
-        if(dp[i] != -1){
-            return dp[i];
-        }
+        return dp[n];
 
-        int take = nums[i] + solve(i + 2, nums, dp);
-        int notTake = solve(i + 1, nums, dp);
-
-        return dp[i] = Math.max(take,notTake);
+        
     }
+
+    // public int solve(int i , int [] nums , int [] dp){
+    //     int n = nums.length;
+    //     if(i >= n){
+    //         return 0;
+    //     }
+
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
+
+    //     int take = nums[i] + solve(i + 2, nums, dp);
+    //     int notTake = solve(i + 1, nums, dp);
+
+    //     return dp[i] = Math.max(take,notTake);
+    // }
     
     
 }
