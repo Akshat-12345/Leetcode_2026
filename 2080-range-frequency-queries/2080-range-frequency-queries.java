9ef127373254f1a5bcef7a97166a02f073f73d4e
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-03-18 11:39:41 +0530

    Time: 185 ms (17.3%), Space: 267.5 MB (10.27%) - LeetHub

--- 2080-range-frequency-queries/2080-range-frequency-queries.java
+++ 2080-range-frequency-queries/2080-range-frequency-queries.java
@@ -1,57 +1,54 @@
-import java.util.*;
-
 class RangeFreqQuery {
-
-    Map<Integer, Integer>[] st;
-    int n;
+    int [] nums;
+    Map<Integer,Integer> [] st;
 
     public RangeFreqQuery(int[] arr) {
-        n = arr.length;
+        this.nums = arr;
+        int n = arr.length;
         st = new HashMap[4 * n];
-        build(0, 0, n - 1, arr);
+        buildTree(0,0,n-1);
     }
-
-    private void build(int node, int l, int r, int[] arr) {
-        st[node] = new HashMap<>();
-
-        if (l == r) {
-            st[node].put(arr[l], 1);
+    
+    public void buildTree(int i , int s, int e){
+        st[i] = new HashMap<>();
+        if(s == e){
+            st[i].put(nums[s],1);
             return;
         }
 
-        int mid = (l + r) / 2;
+        int mid = (s+e)/2;
 
-        build(2 * node + 1, l, mid, arr);
-        build(2 * node + 2, mid + 1, r, arr);
+        buildTree(2*i+1,s,mid);
+        buildTree(2*i+2,mid+1,e);
 
-        // merge left + right
-        Map<Integer, Integer> left = st[2 * node + 1];
-        Map<Integer, Integer> right = st[2 * node + 2];
+        Map<Integer, Integer> left = st[2 *i + 1];
+        Map<Integer, Integer> right = st[2 * i + 2];
 
-        for (int key : left.keySet()) {
-            st[node].put(key, left.get(key));
+        for(int key : left.keySet()){
+            st[i].put(key,left.get(key));
         }
 
         for (int key : right.keySet()) {
-            st[node].put(key, st[node].getOrDefault(key, 0) + right.get(key));
+            st[i].put(key, st[i].getOrDefault(key, 0) + right.get(key));
         }
     }
 
     public int query(int left, int right, int value) {
-        return query(0, 0, n - 1, left, right, value);
+        return getElement(0 , 0 , nums.length - 1, left, right, value);
     }
 
-    private int query(int node, int l, int r, int ql, int qr, int value) {
-        if (r < ql || l > qr) return 0;
+    public int getElement(int i , int s, int e, int l , int r, int val){
+        if(e < l || s > r){
+            return 0;
+        }else if( s >= l && e <= r){
+            return st[i].getOrDefault(val,0);
+        }else{
+            int mid = (s+e)/2;
+            int li = getElement(2*i+1,s,mid,l,r,val);
+            int ri = getElement(2*i+2,mid+1,e,l,r,val);
 
-        if (ql <= l && r <= qr) {
-            return st[node].getOrDefault(value, 0);
+            return li + ri;
         }
-
-        int mid = (l + r) / 2;
-
-        return query(2 * node + 1, l, mid, ql, qr, value)
-             + query(2 * node + 2, mid + 1, r, ql, qr, value);
     }
 }
 
