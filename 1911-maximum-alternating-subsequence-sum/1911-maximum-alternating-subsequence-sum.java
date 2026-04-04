bd557d4b99490c949a73ab0ac1296b8b86b2750b
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-04-04 17:24:57 +0530

    Time: 66 ms (20.85%), Space: 123.6 MB (19.05%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -1,16 +1,45 @@
 class Solution {
+    // public long maxAlternatingSum(int[] nums) {
+    //     int n = nums.length;
+    //     long [][] dp = new long [n + 1][2];
+
+    //     dp[0][0] = 0;
+    //     dp[0][1] = 0;
+
+    //     for(int i = 1 ; i < n + 1 ; i++){
+    //         dp[i][0] = Math.max(dp[i-1][0], dp[i -1][1] + nums[i - 1]);
+    //         dp[i][1] = Math.max(dp[i-1][1], dp[i -1][0] - nums[i - 1]);
+    //     } 
+
+    //     return dp[n][0];
+    // }
+
     public long maxAlternatingSum(int[] nums) {
         int n = nums.length;
+        
         long [][] dp = new long [n + 1][2];
+        for (int i = 0; i <= n; i++) {
+           Arrays.fill(dp[i], -1);
+        }
 
-        dp[0][0] = 0;
-        dp[0][1] = 0;
+        return solve(0 , 0 , nums , dp);
+    }
 
-        for(int i = 1 ; i < n + 1 ; i++){
-            dp[i][0] = Math.max(dp[i-1][0], dp[i -1][1] + nums[i - 1]);
-            dp[i][1] = Math.max(dp[i-1][1], dp[i -1][0] - nums[i - 1]);
-        } 
+    public long solve(int i , int state , int [] nums , long [][] dp){
+        int n = nums.length;
 
-        return dp[n][0];
+        if(i == n){
+            return 0;
+        }
+
+        if(dp[i][state] != -1){
+            return dp[i][state];
+        }
+
+        if(state == 0){
+            return dp[i][state] = Math.max(solve(i + 1, 1 , nums , dp) + nums[i] , solve(i + 1, 0 , nums , dp));
+        }else{
+            return dp[i][state] = Math.max(solve(i + 1, 0 , nums , dp) - nums[i] , solve(i + 1, 1, nums , dp));
+        }
     }
 } 
