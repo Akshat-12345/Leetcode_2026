04c2776449ed4c58ed576df75a8b9bd1474912f2
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-04-30 16:28:10 +0530

    Time: 109 ms (10.28%), Space: 75.2 MB (21.81%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,4 +1,38 @@
 class Solution {
+    // public boolean canPartition(int[] nums) {
+    //     int sum = 0;
+    //     int n = nums.length;
+
+    //     for(int i = 0 ; i < n ; i++){
+    //         sum += nums[i];
+    //     }
+
+    //     if(sum % 2 == 1){
+    //         return false;
+    //     }
+        
+    //     int target = sum/ 2;
+
+    //     boolean [][] dp = new boolean[n + 1][target + 1];
+
+    //     for(int i = 0; i <= n ; i++){
+    //         dp[i][0] = true;
+    //     }
+
+    //     for(int i = 1; i <= n ; i++){
+    //         for(int j = 1 ; j <= target ; j++){
+    //             int val = nums[i-1];
+    //             if(j >= val){
+    //                dp[i][j] = dp[i-1][j] || dp[i - 1][j - val];
+    //             }else{
+    //                 dp[i][j] = dp[i-1][j];
+    //             }
+    //         }
+    //     }
+        
+    //     return dp[n][target];
+    // }
+
     public boolean canPartition(int[] nums) {
         int sum = 0;
         int n = nums.length;
@@ -13,23 +47,31 @@ class Solution {
         
         int target = sum/ 2;
 
-        boolean [][] dp = new boolean[n + 1][target + 1];
+        Boolean [][] dp = new Boolean[n + 1][target + 1];
+        return solve(0 , target , nums , dp);
+    }
 
-        for(int i = 0; i <= n ; i++){
-            dp[i][0] = true;
+    public boolean solve(int i , int target , int [] nums, Boolean[][] dp){
+        int n = nums.length;
+        if(i == n){
+            return false;
         }
 
-        for(int i = 1; i <= n ; i++){
-            for(int j = 1 ; j <= target ; j++){
-                int val = nums[i-1];
-                if(j >= val){
-                   dp[i][j] = dp[i-1][j] || dp[i - 1][j - val];
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
+        if(target == 0){
+            return true;
         }
-        
-        return dp[n][target];
+
+        if(dp[i][target] != null){
+            return dp[i][target];
+        }
+
+        boolean take = false;
+        if(target >= nums[i]){
+            take = solve(i + 1 , target - nums[i], nums, dp);
+        }
+
+        boolean notTake = solve(i + 1 , target, nums, dp);
+
+        return dp[i][target] = take || notTake;
     }
 }
