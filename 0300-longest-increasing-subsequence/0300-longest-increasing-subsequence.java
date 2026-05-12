d0cad72894f09b76071b26c8e04f38cbc66f1cee
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-05-12 10:08:55 +0530

    Time: 440 ms (5.02%), Space: 169.4 MB (19.57%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,49 @@
+class Solution {
+    int [][] dp;
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0 ; i < n ; i++){
+            set.add(nums[i]);
+        }
+
+        int m = set.size();
+        int [] arr = new int [m];
+        int k = 0;
+        for(int x : set){
+          arr[k] = x;
+          k++;
+        }
+        Arrays.sort(arr);
+
+        dp = new int [m + 1][n + 1];
+
+        for(int i = 0 ; i <= m ; i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return solve(0,0,arr,nums);
+
+    }
+
+    public int solve(int i , int j , int [] arr, int [] nums){
+        int n = arr.length;
+        int m = nums.length;
+        if(i == n || j == m ){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        int take = 0;
+        if(arr[i] == nums[j]){
+            take = 1 + solve(i +1, j + 1, arr, nums);
+        }
+
+        int notTake = Math.max(solve(i + 1, j , arr, nums), solve (i , j + 1, arr, nums));
+
+        return dp[i][j] = Math.max(take,notTake);
+    }
+}
