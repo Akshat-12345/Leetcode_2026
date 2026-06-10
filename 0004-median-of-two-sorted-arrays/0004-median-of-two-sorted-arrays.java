6aff530f9aec1e6746fd16e9f9c3846f3906a6aa
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-06-10 17:01:52 +0530

    Time: 1 ms (100%), Space: 49 MB (31.09%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,50 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
-        // Step 1: ensure nums1 is smaller
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
 
-        int n = nums1.length;
-        int m = nums2.length;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
 
-        int low = 0, high = n;
+        int total = n1 + n2;
+        int i = 0, j = 0;
 
-        while (low <= high) {
+        int prev = 0, curr = 0;
 
-            int cut1 = (low + high) / 2;
-            int cut2 = (n + m + 1) / 2 - cut1;
+        // Median position tak hi traverse karna hai
+        for (int count = 0; count <= total / 2; count++) {
 
-            int left1  = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            int right1 = (cut1 == n) ? Integer.MAX_VALUE : nums1[cut1];
+            prev = curr;
 
-            int left2  = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
-            int right2 = (cut2 == m) ? Integer.MAX_VALUE : nums2[cut2];
-
-            // correct partition
-            if (left1 <= right2 && left2 <= right1) {
-
-                // odd length
-                if ((n + m) % 2 == 1) {
-                    return Math.max(left1, left2);
-                }
-
-                // even length
-                return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
-            }
-
-            // move left
-            else if (left1 > right2) {
-                high = cut1 - 1;
-            }
-
-            // move right
-            else {
-                low = cut1 + 1;
+            if (i < n1 && (j >= n2 || nums1[i] <= nums2[j])) {
+                curr = nums1[i];
+                i++;
+            } else {
+                curr = nums2[j];
+                j++;
             }
         }
 
-        return 0.0; // should never reach here
+        // Odd length
+        if (total % 2 == 1) {
+            return curr;
+        }
+
+        // Even length
+        return (prev + curr) / 2.0;
     }
 }
