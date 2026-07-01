05983b3a5434736ab5593bb6b1081c598f8a48ee
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-07-01 11:30:09 +0530

    Time: 6 ms (85.89%), Space: 46.2 MB (41.8%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,46 +1,28 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int [][] dp = new int [n + 1][n + 1];
 
-        for(int i = n - 1 ; i >= 0 ; i--){
-            for(int j = i -1 ; j >= -1 ; j--){
-                int take = 0;
-                if( j == -1 || nums[i] > nums[j] ){
-                    take = 1 + dp[i + 1][i + 1];
-                }
-                int notTake = dp[i + 1][j  + 1];
+        ArrayList<Integer> tail = new ArrayList<>();
 
-                dp[i][j + 1] = Math.max(take,notTake);
+        for (int num : nums) {
+
+            int left = 0;
+            int right = tail.size();
+
+            while (left < right) {
+                int mid = (left + right) / 2;
+
+                if (tail.get(mid) < num)
+                    left = mid + 1;
+                else
+                    right = mid;
             }
+
+            if (left == tail.size())
+                tail.add(num);
+            else
+                tail.set(left, num);
         }
 
-        return dp[0][0];
+        return tail.size();
     }
-
-     // public int lengthOfLIS(int[] nums) {
-    //     int n = nums.length;
-    //     Integer [][] dp = new Integer [n + 1][n + 1];
-    //     return solve(0 , -1, nums,dp);
-    // }
-
-    // public int solve(int i , int prev , int [] nums , Integer [][] dp){
-    //     int n = nums.length;
-    //     if(i == n){
-    //         return 0;
-    //     }
-
-    //     if(dp[i][prev + 1] != null){
-    //         return dp[i][prev + 1];
-    //     }
-        
-    //     int take = 0;
-    //     if(prev == -1 || nums[i] > nums[prev]){
-    //        take = 1 + solve(i + 1 , i , nums , dp);
-    //     }
-
-    //     int notTake = solve(i + 1 , prev , nums,dp);
-
-    //     return dp[i][prev + 1] = Math.max(take,notTake); 
-    // }
 }
