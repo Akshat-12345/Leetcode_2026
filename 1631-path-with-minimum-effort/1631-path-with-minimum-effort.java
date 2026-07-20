77d31cd8c83bd93cfde12b0b96059fa5456e9d66
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-07-20 16:56:07 +0530

    Time: 55 ms (32.25%), Space: 48.1 MB (25.96%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,55 +1,50 @@
 class Solution {
     class pair{
         int from;
-        int to;int wt;
-        public pair(int from, int to, int wt){
+        int to;
+        int wt;
+        public pair(int from , int to , int wt){
             this.from = from;
             this.to = to;
             this.wt = wt;
         }
     }
-
-    int maxDist = 0;
-
+    
     int [] rows = {-1,1,0,0};
-    int [] cols= {0,0,-1,1};
+    int [] cols = {0,0,-1,1};
 
     public int minimumEffortPath(int[][] heights) {
         int n = heights.length;
         int m = heights[0].length;
-        
-        int [][] effort = new int[n][m];
-        for(int i = 0 ; i< n ; i++){
-            Arrays.fill(effort[i],Integer.MAX_VALUE);
+
+        int [][] dist = new int [n][m];
+
+        for(int i = 0 ; i < n ; i++){
+            Arrays.fill(dist[i],Integer.MAX_VALUE);
         }
-        effort[0][0] = 0;
 
-        PriorityQueue<pair> pq = new PriorityQueue<>((a, b) -> a.wt - b.wt);
+        dist[0][0] = 0;
+        PriorityQueue<pair> pq = new PriorityQueue<>((a,b) -> a.wt - b.wt);
+        
         pq.add(new pair(0,0,0));
-
 
         while(!pq.isEmpty()){
             pair curr = pq.poll();
-            int currRow = curr.from;
-            int currCol = curr.to;
-            int currDist = curr.wt;
 
             for(int i = 0 ; i < 4 ; i++){
-                int row = currRow + rows[i];
-                int col = currCol + cols[i];
+                int cr = curr.from + rows[i];
+                int cc = curr.to + cols[i];
 
-                if(row >= 0 && row < n && col >= 0 && col < m ){
-
-                    int eff = Math.max(currDist,Math.abs(heights[currRow][currCol] - heights[row][col]));
-                    if(eff < effort[row][col]){
-                        effort[row][col] = eff;
-                        pq.add(new pair(row,col,eff));
-                    }            
+                if(cr >= 0 && cr < n && cc >= 0 && cc < m ){
+                    int val = Math.max(curr.wt,Math.abs(heights[cr][cc] - heights[curr.from][curr.to]));
+                    if( val < dist[cr][cc]){
+                        pq.add(new pair(cr,cc,val));
+                        dist[cr][cc] = val;
+                    }
                 }
             }
         }
 
-        return effort[n-1][m-1];
-
+        return dist[n-1][m-1];
     }
 }
