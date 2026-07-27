455f8db99f8f447c56a500a5ced0030801b4fc8b
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-07-27 10:44:27 +0530

    Time: 45 ms (59.83%), Space: 47.5 MB (58.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,77 +1,33 @@
 class Solution {
-    // public boolean canPartition(int[] nums) {
-    //     int sum = 0;
-    //     int n = nums.length;
-
-    //     for(int i = 0 ; i < n ; i++){
-    //         sum += nums[i];
-    //     }
-
-    //     if(sum % 2 == 1){
-    //         return false;
-    //     }
-        
-    //     int target = sum/ 2;
-
-    //     boolean [][] dp = new boolean[n + 1][target + 1];
-
-    //     for(int i = 0; i <= n ; i++){
-    //         dp[i][0] = true;
-    //     }
-
-    //     for(int i = 1; i <= n ; i++){
-    //         for(int j = 1 ; j <= target ; j++){
-    //             int val = nums[i-1];
-    //             if(j >= val){
-    //                dp[i][j] = dp[i-1][j] || dp[i - 1][j - val];
-    //             }else{
-    //                 dp[i][j] = dp[i-1][j];
-    //             }
-    //         }
-    //     }
-        
-    //     return dp[n][target];
-    // }
-
     public boolean canPartition(int[] nums) {
-        int sum = 0;
         int n = nums.length;
 
-        for(int i = 0 ; i < n ; i++){
-            sum += nums[i];
+        int sum = 0 ; 
+        for(int num : nums){
+            sum += num;
         }
-
+        
         if(sum % 2 == 1){
             return false;
         }
+
+        boolean [][] dp = new boolean [n + 1][sum/2 + 1];
         
-        int target = sum/ 2;
-
-        Boolean [][] dp = new Boolean[n + 1][target + 1];
-        return solve(0 , target , nums , dp);
-    }
-
-    public boolean solve(int i , int target , int [] nums, Boolean[][] dp){
-        int n = nums.length;
-        if(i == n){
-            return false;
+        for(int i = 0 ; i <= n ;i++){
+            dp[i][0] = true;
         }
 
-        if(target == 0){
-            return true;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= sum/2 ; j++){
+                int val = nums[i-1];
+                if(val <= j && dp[i-1][j - val] == true){
+                    dp[i][j] = true;
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
 
-        if(dp[i][target] != null){
-            return dp[i][target];
-        }
-
-        boolean take = false;
-        if(target >= nums[i]){
-            take = solve(i + 1 , target - nums[i], nums, dp);
-        }
-
-        boolean notTake = solve(i + 1 , target, nums, dp);
-
-        return dp[i][target] = take || notTake;
+        return dp[n][sum/2];
     }
 }
